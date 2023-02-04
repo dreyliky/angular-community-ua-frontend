da395b31455dc1531008e3c8064e80f43ab25d4a
DreyLiky <dreyliky@gmail.com>
2023-02-04 15:14:02 +0200

    fix: projects by eslint

--- apps/code-review-e2e/src/support/commands.ts
+++ apps/code-review-e2e/src/support/commands.ts
@@ -10,10 +10,10 @@
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Chainable<Subject> {
-    login(email: string, password: string): void;
-  }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> {
+        login(email: string, password: string): void;
+    }
 }
 //
 // -- This is a parent command --
