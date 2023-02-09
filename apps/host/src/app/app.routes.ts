78d0697e15bd1a3115e7b83f55fc9e3abdf8cfd3
Andrew Buvria <buvrya228@gmail.com>
2023-02-09 13:45:27 +0100

    fix(host): auth module didn't loaded

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { AppRouteEnum } from './core/enums/app-route.enum';
 
 export const APP_ROUTES: Route[] = [
     {
-        path: AppRouteEnum.Home,
-        loadChildren: () =>
-            import('./pages/home/home.module').then((m) => m.HomeModule)
-    },
-    {
         path: AppRouteEnum.Auth,
         loadChildren: () =>
             import('./pages/auth/auth.module').then((m) => m.AuthModule)
+    },
+    {
+        path: AppRouteEnum.Home,
+        loadChildren: () =>
+            import('./pages/home/home.module').then((m) => m.HomeModule)
     },
     {
         path: '**',
