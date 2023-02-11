5bb2b01095ea655025af50a2eeb9a447fea9a290
Andrew Buvria <buvrya228@gmail.com>
2023-02-11 20:29:02 +0100

    feat(host): add enviroments

--- apps/host/src/app/pages/auth/components/login/login.component.ts
+++ apps/host/src/app/pages/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { LoginWidgetEnum } from './../../../../features/login/enums/login-widget.enum';
-import { LoginWidgetConfig } from './../../../../features/login/interfaces/login-widget.interface';
+import { LoginWidgetEnum } from '@host/features/login/enums/login-widget.enum';
+import { LoginWidgetConfig } from '@host/features/login/interfaces/login-widget.interface';
+import { environment } from 'apps/host/src/environments/environment';
 
 @Component({
     selector: 'acua-login',
@@ -11,7 +12,7 @@ import { LoginWidgetConfig } from './../../../../features/login/interfaces/login
 export class LoginComponent {
     public widgetConfig: LoginWidgetConfig = {
         'src': 'https://telegram.org/js/telegram-widget.js?21',
-        'data-telegram-login': 'acua_dev_bot',
+        'data-telegram-login': environment['BotLoginName'],
         'data-size': 'large',
         'data-request-access': 'write',
         'data-onauth': `${LoginWidgetEnum.BotCallbackName}(user)`
---
+++ apps/host/src/environments/environment.local.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    BotLoginName: 'acua_dev_bot'
+};
