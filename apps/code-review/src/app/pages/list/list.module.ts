4a2da00e7c58750c053f1476ace51894bb67d9b5
br1ttan <brittanvladislav@gmail.com>
2023-02-12 15:31:01 +0200

    chore(code-review): changed imports

--- apps/code-review/src/app/pages/list/list.module.ts
+++ apps/code-review/src/app/pages/list/list.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
-import { AllRequestsComponent } from './components/all-requests';
-import { HistoricalRequestsComponent } from './components/historical-requests';
+import {
+    AllRequestsComponent,
+    HistoricalRequestsComponent,
+    MyRequestsComponent
+} from './components';
 import { ListComponent } from './list.component';
 import { LIST_ROUTES } from './list.routes';
-import { MyRequestsComponent } from './components/my-requests';
 
 @NgModule({
     declarations: [
