fe9609d0f1d1777b1ac7d30c9b73f569741b0e7a
DreyLiky <dreyliky@gmail.com>
2023-02-13 20:10:43 +0200

    refactor: change stackblitz editor to monaco

--- apps/code-review/src/app/app.component.ts
+++ apps/code-review/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'acua-root',
+    styleUrls: ['./app.component.scss'],
     template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {}
