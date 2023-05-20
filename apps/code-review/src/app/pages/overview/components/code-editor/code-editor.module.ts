004fb64d1f7a6161e420487b3213a2e1b97f287d
dreyliky <dreyliky@gmail.com>
2023-05-20 18:50:00 +0300

    chore: update styles for comments amount

--- apps/code-review/src/app/pages/overview/components/code-editor/code-editor.module.ts
+++ apps/code-review/src/app/pages/overview/components/code-editor/code-editor.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 import { CodeEditorComponent } from './code-editor.component';
 import { CommentsAmountComponent } from './components';
 
 @NgModule({
     declarations: [CodeEditorComponent, CommentsAmountComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatIconModule, MatBadgeModule],
     exports: [CodeEditorComponent]
 })
 export class CodeEditorModule {}
